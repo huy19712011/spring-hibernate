654590413d53c71ac13ce15464144036917a3229
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-27 18:35:04 +0700

    Begin of Section 25 - Eager vs Lazy loading - set up

---
+++ src/main/java/hb_03_eager_vs_lazy_demo/CreateCoursesDemo.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hb_03_eager_vs_lazy_demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author huynq
+ */
+public class CreateCoursesDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // create Session factory
+        SessionFactory factory
+                = new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Instructor.class)
+                        .addAnnotatedClass(InstructorDetail.class)
+                        .addAnnotatedClass(Course.class)
+                        .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the instructor from db
+            int id = 1;
+            Instructor instructor =
+                    session.get(Instructor.class, id);
+
+            // create some courses
+            Course course1 = new Course("Angular");
+            Course course2 = new Course("React");
+
+            // add courses to instructor
+            instructor.add(course1);
+            instructor.add(course2);
+
+            // save the courses
+            session.save(course1);
+            session.save(course2);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!!!");
+
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+
+        }
+    }
+
+}
---
+++ src/main/java/hb_03_eager_vs_lazy_demo/CreateInstructorDemo.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hb_03_eager_vs_lazy_demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author huynq
+ */
+public class CreateInstructorDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // create the objects
+            Instructor instructor =
+                    new Instructor("Susan", "Public", "susan@gmail.com");
+
+            InstructorDetail instructorDetail=
+                    new InstructorDetail("http://www.youtube.com", "Music");
+
+            // associate the objects
+            instructor.setInstructorDetail(instructorDetail);
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the instructor
+            // this also save the details object because of CascadeTpe.ALL
+            System.out.println("Saving instructor: " + instructor);
+            session.save(instructor);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!!!");
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+
+}
