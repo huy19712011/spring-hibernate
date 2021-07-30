d981418820d7c16267b9b77774472b658e4b6a6e
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-30 10:19:41 +0700

    221: @OneToOne bi-directional - Get and Delete

---
+++ src/main/java/T1_C23_one_to_one_bidirectional/InstructorDetail.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package T1_C23_one_to_one_bidirectional;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author huynq
+ */
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    private Instructor instructor;
+
+
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" + "id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + '}';
+    }
+
+
+}
---
+++ src/main/java/T1_C23_one_to_one_bidirectional/MainGetOneToOneBi.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package T1_C23_one_to_one_bidirectional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author huynq
+ */
+public class MainGetOneToOneBi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // create Session factory
+        SessionFactory factory
+                = new Configuration()
+                        .configure("hibernate.cfg_C23_uni.xml")
+                        .addAnnotatedClass(Instructor.class)
+                        .addAnnotatedClass(InstructorDetail.class)
+                        .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the instructor-detail object
+            int id = 2;
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class, id);
+            System.out.println("instructorDetail: " + instructorDetail);
+
+            // associated instructor
+            System.out.println("The associated instructor: " + instructorDetail.getInstructor());
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!!!");
+
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+
+}
