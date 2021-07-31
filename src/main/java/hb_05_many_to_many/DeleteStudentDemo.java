1391ca3579dd274b5df1b2e070aab540a6e25b33
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-31 16:09:02 +0700

    @ManyToMany - Delete a student

---
+++ src/main/java/hb_05_many_to_many/DeleteStudentDemo.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hb_05_many_to_many;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author huynq
+ */
+public class DeleteStudentDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // create Session factory
+        SessionFactory factory
+                = new Configuration()
+                        .configure("hibernate.cfg_C27_many_to_many.xml")
+                        .addAnnotatedClass(Instructor.class)
+                        .addAnnotatedClass(InstructorDetail.class)
+                        .addAnnotatedClass(Course.class)
+                        .addAnnotatedClass(Review.class)
+                        .addAnnotatedClass(Student.class)
+                        .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the student from db
+            int id = 2;
+            Student student = session.get(Student.class, id);
+            System.out.println("Loaded student: " + student);
+            System.out.println("Courses: " + student.getCourses());
+
+            // delete a student
+            System.out.println("\nDeleting student: " + student);
+            session.delete(student);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!!!");
+
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+
+}
