fc593cadd40a533867a9a4e52d5a13fc204fa58b
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-31 10:42:07 +0700

    249: @OneToMany Uni-Directional - Getting

---
+++ src/main/java/hb_04_one_to_many_uni/GetCourseAndReviewsDemo.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hb_04_one_to_many_uni;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author huynq
+ */
+public class GetCourseAndReviewsDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // create Session factory
+        SessionFactory factory
+                = new Configuration()
+                        .configure("hibernate.cfg_C26_uni.xml")
+                        .addAnnotatedClass(Instructor.class)
+                        .addAnnotatedClass(InstructorDetail.class)
+                        .addAnnotatedClass(Course.class)
+                        .addAnnotatedClass(Review.class)
+                        .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the course
+            int id = 10;
+            Course course = session.get(Course.class, id);
+
+            //
+            System.out.println(course);
+
+            //
+            System.out.println(course.getReviews());
+
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!!!");
+
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+
+}
